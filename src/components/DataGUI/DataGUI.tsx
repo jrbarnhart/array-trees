e1183a23dfa4b1b55dbc96592852c8141eb08ea1
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-09-08 18:30:22 -0400

    Add header labels and adjust style

--- src/components/DataGUI/DataGUI.tsx
+++ src/components/DataGUI/DataGUI.tsx
@@ -24,19 +24,32 @@ export default function DataGUI({
           </CardDescription>
         </CardHeader>
         <CardContent className="grid grid-rows-[min-content_1fr] grid-cols-2 gap-x-2">
-          <h2 className="text-lg font-bold">Data</h2>
-          <ScrollArea className="h-48 border shadow-inner rounded-sm">
+          <h2 className="text-lg font-bold">
+            Data{" "}
+            <span className="text-xs text-muted-foreground">Index - Data</span>
+          </h2>
+          <ScrollArea className="h-48 p-1 w-full border shadow-inner rounded-sm">
             <ul>
               {data.map((entry, index) => (
-                <li key={index}>{entry}</li>
+                <li
+                  className="whitespace-nowrap"
+                  key={index}
+                >{`${index.toString()} - ${entry}`}</li>
               ))}
             </ul>
           </ScrollArea>
-          <h2 className="col-start-2 row-start-1 text-lg font-bold">Edges</h2>
-          <ScrollArea className="h-48 border shadow-inner rounded-sm">
+          <h2 className="col-start-2 row-start-1 text-lg font-bold">
+            Edges{" "}
+            <span className="text-xs text-muted-foreground">
+              Parent - Child
+            </span>
+          </h2>
+          <ScrollArea className="h-48 p-1 border shadow-inner rounded-sm">
             <ul>
               {edges.map((entry, index) => (
-                <li key={index}>{entry}</li>
+                <li
+                  key={index}
+                >{`${entry[0].toString()} - ${entry[1].toString()}`}</li>
               ))}
             </ul>
           </ScrollArea>
